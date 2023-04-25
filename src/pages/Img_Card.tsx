a085d72c43fa64c3d331b5b43e40bb8c9f0cdc96
Greg <greg@sheb.rocks>
2023-04-25 10:03:34 +0200

    install and run prettier

--- src/pages/Img_Card.tsx
+++ src/pages/Img_Card.tsx
@@ -1,93 +1,136 @@
-import {useEffect, useState} from "react";
-import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+import { useEffect, useState } from "react";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
 import Image from "next/image";
 
-export default function ImageCard({directusData: cardData, setDirectusData: setCardData, publicData}) {
-    const [isInEditMode, setIsInEditMode] = useState<number[]>([])
-    const [isCardUpdated, setIsCardUpdated] = useState(false);
+export default function ImageCard({
+  directusData: cardData,
+  setDirectusData: setCardData,
+  publicData,
+}) {
+  const [isInEditMode, setIsInEditMode] = useState<number[]>([]);
+  const [isCardUpdated, setIsCardUpdated] = useState(false);
 
+  const handleRatingRange = (event, panda) => {
+    const updatedPandas = cardData.map((uPanda) => {
+      if (uPanda.id === panda.id) {
+        const updatedPanda = { ...uPanda, rating: event.target.value };
+        setIsCardUpdated(
+          JSON.stringify(updatedPanda) !== JSON.stringify(uPanda)
+        );
+        return updatedPanda;
+      }
+      return uPanda;
+    });
+    setCardData(updatedPandas);
+  };
 
-
-    const handleRatingRange = (event, panda) => {
-        const updatedPandas = cardData.map((uPanda) => {
-            if (uPanda.id === panda.id) {
-                const updatedPanda = { ...uPanda, rating: event.target.value };
-                setIsCardUpdated(JSON.stringify(updatedPanda) !== JSON.stringify(uPanda));
-                return updatedPanda
-            }
-            return uPanda;
-        });
-        setCardData(updatedPandas);
-    };
-
-    const saveUpdatedData = () => {
-        const updateDataMutation = `
+  const saveUpdatedData = () => {
+    const updateDataMutation = `
                mutation updatePageItem($id: ID!, $name: String!, $rating: JSON! ){
                 update_pages_item(id: $id, data: {name_input: $name, rating: $rating}) {
                     id
                     name_input
                     rating
-                    
+                       
                 }
             }
     
     `;
-    }
-    const openEditForm = (cardId: number) => {
-        setIsInEditMode([...isInEditMode,
-        cardId])
+  };
+  const openEditForm = (cardId: number) => {
+    setIsInEditMode([...isInEditMode, cardId]);
+  };
+  const closeEditForm = (cardId: number) => {
+    setIsInEditMode(isInEditMode.filter((id) => id !== cardId));
+  };
 
-    };
-    const closeEditForm = (cardId : number) => {
-        setIsInEditMode(isInEditMode.filter((id) => id !== cardId))
-    }
-
-
-
-    return (
-        <div className='grid grid-cols-2'>
-            {cardData?.map((panda) => {
-                return (
-                    <div key={panda.id} className='m-20 justify-center bg-amber-50 p-20 rounded-2xl'>
-                    {isInEditMode.includes(panda.id) ?  (<div>
-
-
-                        <form className='flex-col'>
-                            <div>
-                        <label className='m-2'>New Name</label>
-                            <input/>
-                            </div>
-                        <label> Cuteness lvl:
-                        </label>
-                            <input type={"range"} min={1} max={7} value={panda.rating} onChange={(event) => handleRatingRange(event, panda)}/>
-                        </form>
-                        <div className='flex justify-between'>
-                        <button className='bg-red-500 p-2 rounded-xl ' onClick={() => closeEditForm(panda.id)}>cancel</button>
-                        <button className='bg-red-500 p-2 rounded-xl ' type={'submit'}>save</button>
-                        </div>
-                    </div>) :(
-                                <div>
-                                    <button className='bg-red-500 p-4 rounded-xl ' onClick={() => openEditForm(panda.id)}>Edit</button>
-                                <p>ID: {panda.id}</p>
-                                <p>Name: {panda.name_input}</p>
-                                <p>Cuteness: {panda.rating}</p>
-                                {panda.image ? (
-                                <Image className='rounded-2xl' src={'http://localhost:8055/assets/' + panda.image} alt={'img'} width={450} height={450} />
-                                ) : null }
-                            <div className='flex justify-end'>
-                                <p>{new Date(panda.date_created).toLocaleDateString('de-de', ) } -</p>
-                                <p>{new Date(panda.date_created).toLocaleTimeString('de-de', {hour: 'numeric', minute: 'numeric'}) }</p>
-                            </div>
-                                </div>
-                        )}
-
-                    </div>
-
-                )
-            })}
-            <button disabled={!isCardUpdated} onClick={() => console.log('gespeichert')} className='m-4 p-4 bg-amber-100 rounded-2xl'>
-                Speichern
-            </button>
-        </div>
-    )
+  return (
+    <div className="grid grid-cols-2">
+      {cardData?.map((panda) => {
+        return (
+          <div
+            key={panda.id}
+            className="m-20 justify-center bg-amber-50 p-20 rounded-2xl"
+          >
+            {isInEditMode.includes(panda.id) ? (
+              <div>
+                <form className="flex-col">
+                  <div>
+                    <label className="m-2">New Name</label>
+                    <input />
+                  </div>
+                  <label> Cuteness lvl:</label>
+                  <input
+                    type={"range"}
+                    min={1}
+                    max={7}
+                    value={panda.rating}
+                    onChange={(event) => handleRatingRange(event, panda)}
+                  />
+                </form>
+                <div className="flex justify-between">
+                  <button
+                    className="bg-red-500 p-2 rounded-xl "
+                    onClick={() => closeEditForm(panda.id)}
+                  >
+                    cancel
+                  </button>
+                  <button
+                    className="bg-red-500 p-2 rounded-xl "
+                    type={"submit"}
+                  >
+                    save
+                  </button>
+                </div>
+              </div>
+            ) : (
+              <div>
+                <button
+                  className="bg-red-500 p-4 rounded-xl "
+                  onClick={() => openEditForm(panda.id)}
+                >
+                  Edit
+                </button>
+                <p>ID: {panda.id}</p>
+                <p>Name: {panda.name_input}</p>
+                <p>Cuteness: {panda.rating}</p>
+                {panda.image ? (
+                  <Image
+                    className="rounded-2xl"
+                    src={"http://localhost:8055/assets/" + panda.image}
+                    alt={"img"}
+                    width={450}
+                    height={450}
+                  />
+                ) : null}
+                <div className="flex justify-end">
+                  <p>
+                    {new Date(panda.date_created).toLocaleDateString("de-de")} -
+                  </p>
+                  <p>
+                    {new Date(panda.date_created).toLocaleTimeString("de-de", {
+                      hour: "numeric",
+                      minute: "numeric",
+                    })}
+                  </p>
+                </div>
+              </div>
+            )}
+          </div>
+        );
+      })}
+      <button
+        disabled={!isCardUpdated}
+        onClick={() => console.log("gespeichert")}
+        className="m-4 p-4 bg-amber-100 rounded-2xl"
+      >
+        Speichern
+      </button>
+    </div>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,20 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import { Inter } from "next/font/google";
 import ImageCard from "@/pages/Img_Card";
-import {useState, useEffect} from "react";
-import { Directus, ID } from '@directus/sdk';
-import {render} from "react-dom";
+import { useState, useEffect } from "react";
+import { Directus, ID } from "@directus/sdk";
+import { render } from "react-dom";
 import FormData from "form-data";
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
-const directus = new Directus('http://localhost:8055');
-
-
+const directus = new Directus("http://localhost:8055");
 
 export default function Home() {
-
-        const [directusData, setDirectusData] = useState(null)
-    async function publicData() {
-
-        // We don't need to authenticate if the public role has access to some_public_collection.
-        const pubData = await directus.items('pages').readByQuery();
-        const publicData = await directus.graphql.items(`
+  const [directusData, setDirectusData] = useState(null);
+  async function publicData() {
+    // We don't need to authenticate if the public role has access to some_public_collection.
+    const pubData = await directus.items("pages").readByQuery();
+    const publicData = await directus.graphql.items(`
                     query {
                         pages{
                             id
@@ -32,8 +28,7 @@ export default function Home() {
                             date_created
                         }
                     }
-                `)
-
+                `);
 
     const mutation = `
             mutation {
@@ -44,47 +39,43 @@ export default function Home() {
             }
     
     `;
-        const mutateData = await directus.graphql.items(mutation)
-            .catch(error => console.error('bitte helfen sie mir!'))
+    const mutateData = await directus.graphql
+      .items(mutation)
+      .catch((error) => console.error("bitte helfen sie mir!"));
 
+    setDirectusData(pubData.data);
+  }
 
-        setDirectusData(pubData.data)
-
-    }
-
-    function createPanda() {
-        directus.items('pages').createOne({
-            name_input: 'Panda1'
-        })
-            .then(response => {
-                console.log('New post created:', response)
-                publicData()
-            })
-            .catch(error => console.error('Error creating new post:', error));
-    }
-
-    useEffect(() => {
+  function createPanda() {
+    directus
+      .items("pages")
+      .createOne({
+        name_input: "Panda1",
+      })
+      .then((response) => {
+        console.log("New post created:", response);
         publicData();
+      })
+      .catch((error) => console.error("Error creating new post:", error));
+  }
 
+  useEffect(() => {
+    publicData();
+  }, []);
 
-    }, []);
+  const handleUpdate = async (event) => {
+    event.preventDefault();
 
+    const formData = new FormData(event.target);
+    const name = formData.get("updatedName");
+    const id = 1;
 
+    const variables = {
+      name: name,
+      id: id,
+    };
 
-
-    const handleUpdate = async (event) => {
-        event.preventDefault();
-
-        const formData = new FormData(event.target);
-        const name = formData.get('updatedName');
-        const id = 1;
-
-        const variables = {
-            name: name,
-            id: id
-        };
-
-        const updateMutation = `
+    const updateMutation = `
     mutation updatePageItem($id: ID!, $name: String!) {
       update_pages_item(id: $id, data: { name_input: $name }) {
         id
@@ -93,29 +84,33 @@ export default function Home() {
     }
   `;
 
-        const mutateData = await directus.graphql.items(updateMutation, variables)
-            .catch(error => console.error(error + 'hahlp!'));
+    const mutateData = await directus.graphql
+      .items(updateMutation, variables)
+      .catch((error) => console.error(error + "hahlp!"));
 
-        console.log(mutateData);
-        console.log(directusData)
-        publicData()
-    };
+    console.log(mutateData);
+    console.log(directusData);
+    publicData();
+  };
 
-
-    return (
+  return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
       <h1>Rate pictures of pandas</h1>
-        {/*<button className='rounded-2xl bg-red-500 p-4' onClick={() => createPanda()}>Create new Panda</button>*/}
-        <form onSubmit={handleUpdate} name={'updateMyPanda'}>
-            <div>
-        <label>New Panda Name</label>
-        <input type={"text"} name={'updatedName'}/>
-            </div>
-            <button className='m-4 p-4 rounded-xl bg-gray-400' type={'submit'}>Submit</button>
-        </form>
-        {/*<p>Status: {directusData?[0].rating}</p>*/}
-        <ImageCard directusData={directusData} setDirectusData={setDirectusData}/>
-
+      {/*<button className='rounded-2xl bg-red-500 p-4' onClick={() => createPanda()}>Create new Panda</button>*/}
+      <form onSubmit={handleUpdate} name={"updateMyPanda"}>
+        <div>
+          <label>New Panda Name</label>
+          <input type={"text"} name={"updatedName"} />
+        </div>
+        <button className="m-4 p-4 rounded-xl bg-gray-400" type={"submit"}>
+          Submit
+        </button>
+      </form>
+      {/*<p>Status: {directusData?[0].rating}</p>*/}
+      <ImageCard
+        directusData={directusData}
+        setDirectusData={setDirectusData}
+      />
     </main>
-  )
+  );
 }
