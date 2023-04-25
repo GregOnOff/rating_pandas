8748ed3590210acad55b1ddcf5de991f34e02881
Greg <greg@sheb.rocks>
2023-04-25 10:01:05 +0200

    minor changes

--- src/pages/Img_Card.tsx
+++ src/pages/Img_Card.tsx
@@ -3,17 +3,10 @@ import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client
 import Image from "next/image";
 
 export default function ImageCard({directusData: cardData, setDirectusData: setCardData, publicData}) {
-    const [isEditMode, setIsEditMode] = useState(false)
-    const imgURL = 'http://localhost:8055/assets/'
-
-    const client = new ApolloClient({
-
-        uri: 'http://localhost:8055/graphql',
-        cache: new InMemoryCache(),
-        credentials: 'include',
-
-    });
+    const [isInEditMode, setIsInEditMode] = useState<number[]>([])
     const [isCardUpdated, setIsCardUpdated] = useState(false);
+
+
 
     const handleRatingRange = (event, panda) => {
         const updatedPandas = cardData.map((uPanda) => {
@@ -27,13 +20,37 @@ export default function ImageCard({directusData: cardData, setDirectusData: setC
         setCardData(updatedPandas);
     };
 
+    const saveUpdatedData = () => {
+        const updateDataMutation = `
+               mutation updatePageItem($id: ID!, $name: String!, $rating: JSON! ){
+                update_pages_item(id: $id, data: {name_input: $name, rating: $rating}) {
+                    id
+                    name_input
+                    rating
+                    
+                }
+            }
+    
+    `;
+    }
+    const openEditForm = (cardId: number) => {
+        setIsInEditMode([...isInEditMode,
+        cardId])
+
+    };
+    const closeEditForm = (cardId : number) => {
+        setIsInEditMode(isInEditMode.filter((id) => id !== cardId))
+    }
+
+
 
     return (
         <div className='grid grid-cols-2'>
-            {cardData?.map((panda, index) => {
+            {cardData?.map((panda) => {
                 return (
                     <div key={panda.id} className='m-20 justify-center bg-amber-50 p-20 rounded-2xl'>
-                    {isEditMode ? (<div >
+                    {isInEditMode.includes(panda.id) ?  (<div>
+
 
                         <form className='flex-col'>
                             <div>
@@ -43,15 +60,14 @@ export default function ImageCard({directusData: cardData, setDirectusData: setC
                         <label> Cuteness lvl:
                         </label>
                             <input type={"range"} min={1} max={7} value={panda.rating} onChange={(event) => handleRatingRange(event, panda)}/>
-
                         </form>
                         <div className='flex justify-between'>
-                        <button className='bg-red-500 p-2 rounded-xl ' onClick={() => setIsEditMode(false)}>cancel</button>
+                        <button className='bg-red-500 p-2 rounded-xl ' onClick={() => closeEditForm(panda.id)}>cancel</button>
                         <button className='bg-red-500 p-2 rounded-xl ' type={'submit'}>save</button>
                         </div>
                     </div>) :(
                                 <div>
-                                    <button className='bg-red-500 p-4 rounded-xl ' onClick={() => setIsEditMode(true)}>Edit</button>
+                                    <button className='bg-red-500 p-4 rounded-xl ' onClick={() => openEditForm(panda.id)}>Edit</button>
                                 <p>ID: {panda.id}</p>
                                 <p>Name: {panda.name_input}</p>
                                 <p>Cuteness: {panda.rating}</p>
